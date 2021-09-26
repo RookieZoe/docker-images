4a20263726ed908b41a677fb738ecf83e254c214
Rookie_Zoe <i@rookiezoe.com>
2021-09-26 15:41:41 +0800

    xray: multi-platform build

--- xray/download-xray.sh
+++ xray/download-xray.sh
@@ -3,43 +3,39 @@
 # This is a Shell script for xray based alpine with Docker image
 #
 VERSION=$1
-PLATFORM=$2
+TARGETPLATFORM=$2
 XRAY_REPO="https://github.com/XTLS/Xray-core"
+X_ARCH=
 
-if [ -z "$PLATFORM" ]; then
-  ARCH="64"
-else
-  case "$PLATFORM" in
-  linux/386)
-    ARCH="32"
-    ;;
-  linux/amd64)
-    ARCH="64"
-    ;;
-  linux/arm/v6)
-    ARCH="arm32-v6"
-    ;;
-  linux/arm/v7)
-    ARCH="arm32-v7a"
-    ;;
-  linux/arm64 | linux/arm64/v8)
-    ARCH="arm64-v8a"
-    ;;
-  linux/ppc64le)
-    ARCH="ppc64le"
-    ;;
-  linux/s390x)
-    ARCH="s390x"
-    ;;
-  *)
-    ARCH=""
-    ;;
-  esac
-fi
+echo "TARGETPLATFORM=$TARGETPLATFORM"
 
-[ -z "$ARCH" ] && echo "Error: Not supported OS Architecture" && exit 1
+case "$TARGETPLATFORM" in
+'linux/386')
+  X_ARCH="32"
+  ;;
+'linux/amd64')
+  X_ARCH="64"
+  ;;
+'linux/arm/v6')
+  X_ARCH="arm32-v6"
+  ;;
+'linux/arm/v7')
+  X_ARCH="arm32-v7a"
+  ;;
+'linux/arm64/v8' | 'linux/arm64')
+  X_ARCH="arm64-v8a"
+  ;;
+'linux/ppc64le')
+  X_ARCH="ppc64le"
+  ;;
+'linux/s390x')
+  X_ARCH="s390x"
+  ;;
+esac
+
+[ -z "$X_ARCH" ] && echo "Error: Not supported OS Architecture" && exit 1
 # Download release file
-XRAY_ZIP_FILE="Xray-linux-$ARCH.zip"
+XRAY_ZIP_FILE="Xray-linux-$X_ARCH.zip"
 
 echo "Downloading release file: $XRAY_ZIP_FILE from $XRAY_REPO/releases/download/v$VERSION/$XRAY_ZIP_FILE"
 if ! wget -O /xray.zip "$XRAY_REPO/releases/download/v$VERSION/$XRAY_ZIP_FILE" >/dev/null 2>&1; then
